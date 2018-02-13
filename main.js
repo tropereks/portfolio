140632aa9f798f282f2dcf1abf1d059e3b3217a6
zablocky <jzablocki4096@gmail.com>
2018-02-13 20:57:46 +0100

    1st commit

---
+++ main.js
@@ -0,0 +1,11 @@
+function czyJestWieksze() {
+
+var number1 = document.getElementById(number1).value;
+var number2 = document.getElementById(number2).value;
+var number3 = document.getElementById(number3).value;
+if(number1 < number2 && number2 < number3){
+    document.getElementById(div).innerHTML("liczby rosną");
+}
+else{
+    document.getElementById(div).innerHTML("liczby rosną");
+}}
